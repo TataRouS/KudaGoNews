577c3e75fea0a60140dab19ec81a31b52d1bb067
Nata Kuznetsova <natanavky@icloud.com>
2023-08-23 15:17:12 +0300

    Lesson5

--- ContentView.swift
+++ ContentView.swift
@@ -8,16 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var news: [News] = []
+    private var networkService = NetworkService()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List(news) { news in
+                VStack {
+                    Text(Helper.getDate(date: news.date))
+                        .font(.body)
+                        .fontWeight(.light)
+                    Text(news.title)
+                        .font(.title)
+                        .fontWeight(.heavy)
+                }
+            }
+                .onAppear {
+                    networkService.getNews { (news) in
+                        self.news = news
+                    }
+            }
         }
-        .padding()
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ KudaGoNews/KudaGoNews/Preview Content/NetworkService.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkService.swift
+//  KudaGoNews
+//
+//  Created by Nata Kuznetsova on 23.08.2023.
+//
+
+import Foundation
+
+final class NetworkService {
+    func getNews (completion:@escaping ([News]) -> ()) {
+        guard let url = URL(string: "https://kudago.com/public-api/v1.4/news/?fields=id,title ‚publication_date&text_format=text&actual_only=true")
+            else { return }
+        URLSession.shared.dataTask(with: url) { (data, _, _) in
+            guard let data = data else { return }
+            do {
+                let news = try
+                    JSONDecoder().decode(NewsModel.self, from: data)
+                completion(news.results)
+            } catch {
+                print(error)
+            }
+        }
+        .resume()
+    }
+}
+
